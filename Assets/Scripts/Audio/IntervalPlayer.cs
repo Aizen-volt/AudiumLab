b1a9a075283b85eb374bb6cc06170b125dc1af30
Mateusz Kaszubowski <m.aizenvolt@gmail.com>
2025-11-16 18:33:45 +0100

    Lesson created - intervals

---
+++ Assets/Scripts/Audio/IntervalPlayer.cs
@@ -0,0 +1,86 @@
+﻿// /*
+//  * Copyright © 2025 AudiumLab
+//  * Author: Mateusz Kaszubowski
+//  * Created on: 16/11/2025
+//  */
+
+using UnityEngine;
+
+namespace Audio {
+    [RequireComponent(typeof(AudioSource))]
+    public class IntervalPlayer : MonoBehaviour {
+        [Header("Audio")]
+        [SerializeField] private AudioClip[] intervalClips;
+
+        private AudioSource m_source;
+        private int m_currentIndex;
+        private bool m_isPlaying;
+
+        private void Awake() {
+            m_source = GetComponent<AudioSource>();
+            m_source.playOnAwake = false;
+            m_source.loop = true;
+        }
+
+        private void Start() {
+            SetInterval(0, false);
+        }
+
+        public void OnSliderValueChanged(float value) {
+            int index = Mathf.RoundToInt(value);
+            SetInterval(index, m_isPlaying);
+        }
+
+        private void SetInterval(int index, bool playAfterChange) {
+            if (intervalClips == null || intervalClips.Length == 0) {
+                return;
+            }
+
+            index = Mathf.Clamp(index, 0, Mathf.Min(12, intervalClips.Length - 1));
+            m_currentIndex = index;
+
+            AudioClip clip = intervalClips[index];
+            if (clip == null) {
+                Debug.LogWarning($"[IntervalPlayer] No clip for index {index}.");
+                m_source.Stop();
+                m_isPlaying = false;
+                return;
+            }
+
+            m_source.clip = clip;
+
+            if (playAfterChange) {
+                m_source.Stop();
+                m_source.Play();
+                m_isPlaying = true;
+            }
+        }
+
+        public void OnTogglePlayClicked() {
+            if (m_isPlaying) {
+                StopPlayback();
+            } else {
+                StartPlayback();
+            }
+        }
+
+        public void StartPlayback() {
+            if (m_source.clip == null) {
+                SetInterval(m_currentIndex, false);
+            }
+
+            if (m_source.clip == null) {
+                return;
+            }
+
+            m_source.Stop();
+            m_source.Play();
+            m_isPlaying = true;
+        }
+
+        public void StopPlayback() {
+            m_source.Stop();
+            m_isPlaying = false;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Lessons/Intervals/DisplayIntervalName.cs
@@ -0,0 +1,39 @@
+﻿// /*
+//  * Copyright © 2025 AudiumLab
+//  * Author: Mateusz Kaszubowski
+//  * Created on: 16/11/2025
+//  */
+
+using TMPro;
+using UnityEngine;
+
+namespace UI.Lessons.Intervals {
+    public class DisplayIntervalName : MonoBehaviour {
+        private TextMeshProUGUI text;
+        
+        private void Awake() {
+            text = GetComponent<TextMeshProUGUI>();
+        }
+        
+        public void ChangeIntervalName(float semitones) {
+            int semitonesInt = Mathf.RoundToInt(semitones);
+            string intervalName = semitones switch {
+                0 => "Pryma (0 półtonów)",
+                1 => "Sekunda mała (1 półton)",
+                2 => "Sekunda wielka (2 półtony)",
+                3 => "Tercja mała (3 półtony)",
+                4 => "Tercja wielka (4 półtony)",
+                5 => "Kwarta czysta (5 półtonów)",
+                6 => "Tryton (6 półtonów)",
+                7 => "Kwinta czysta (7 półtonów)",
+                8 => "Seksta mała (8 półtonów)",
+                9 => "Seksta wielka (9 półtonów)",
+                10 => "Septyma mała (10 półtonów)",
+                11 => "Septyma wielka (11 półtonów)",
+                12 => "Oktawa (12 półtonów)",
+                _ => "Nieznany interwał"
+            };
+            text.text = intervalName;
+        }
+    }
+}
