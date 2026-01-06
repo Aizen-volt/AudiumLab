ef72b3712c74c995fde8beadb7edbe2a4c5bb6db
jigglestrudel <tomaszkrepa@outlook.com>
2026-01-06 01:59:33 +0100

    added lesson on quantization

---
+++ Assets/Scripts/Audio/MusicPlayer.cs
@@ -0,0 +1,32 @@
+﻿// /*
+//  * Copyright © 2026 AudiumLab
+//  * Author: Tomasz Krępa
+//  * Created on: 05/01/2026
+//  */
+
+using UnityEngine;
+
+namespace Audio {
+    public class MusicPlayer : MonoBehaviour {
+        
+        private void Awake() {
+            var source = GetComponent<AudioSource>();
+            source.playOnAwake = false;
+            source.loop = false;
+
+            if (VolumeManager.Instance != null) {
+                source.volume = VolumeManager.Instance.Volume;
+            }
+        }
+
+        private void OnEnable() {
+            var source = GetComponent<AudioSource>();
+            source.playOnAwake = false;
+            source.loop = true;
+
+            if (VolumeManager.Instance != null) {
+                source.volume = VolumeManager.Instance.Volume;
+            }
+        }
+    }
+}
